148fcd861493c137285e3a4f5cb3cf0b8fb93e02
wuh0007 <wuh007@udayton.edu>
2020-09-14 14:15:20 -0400

    fifth commit

--- pets/urls.py
+++ pets/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pets, name='pets')
+    path('', views.pets, name='pets'),
+    path('<int:id>', views.pet_detail, name='pet_detail'),
+    path('search', views.search, name='search')
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Pet
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def pets(request):
-    return render(request, 'pets/pets.html')
+    pets = Pet.objects.order_by('-created_date')
+    paginator = Paginator(pets, 4)
+    page = request.GET.get('page')
+    paged_pets = paginator.get_page(page)
+    data = {
+        'pets' : paged_pets,
+    }
+    return render(request, 'pets/pets.html', data)
+
+def pet_detail(request, id):
+    single_pet = get_object_or_404(Pet, pk=id)
+    print(single_pet)
+    data = {
+        'single_pet' : single_pet,
+    }
+    return render(request, 'pets/pet_detail.html', data)
+
+def search(request):
+    pets = Pet.objects.order_by('-created_date')
+
+    # if 'keyword' in request.GET:
+    #     keyword = request.GET['keyword']
+    #     if keyword:
+    #         cars = cars.filter(description__icontains=keyword)
+
+    data = {
+        'pets' : pets,
+    }
+
+    return render(request, 'pets/search.html', data)
